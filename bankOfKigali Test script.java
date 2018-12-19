6ccc1aa06d7d7272c6f27d8f60033eb6dc8f777c
ishimwegloriose <ishimwegloriose@users.noreply.github.com>
2018-12-19 17:16:32 +0200

    Update bankOfKigali Test script.java

--- bankOfKigali Test script.java
+++ bankOfKigali Test script.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package packageBk;
+package packageBkExerc;
 
 
 import java.util.logging.Level;
@@ -14,40 +14,59 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class bankOfKigali {
+
+public class Gloriose {
     
     public static void main(String[] args) {
-        System.getProperty("webdriver.gecko.driver","C:\\Program Files\\selenium-server-standalone\\geckodriver-v0.23.0-win64\\geckodriver.exe");
+
+
+System.getProperty("webdriver.gecko.driver","C:\\Program Files\\selenium-server-standalone\\geckodriver-v0.23.0-win64\\geckodriver.exe");
+
         WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.bk.rw");
+        WebElement link, table;
         
-          driver.get("https://www.bk.rw");
-        WebElement link, link1, link2, link3, link4;
-        //link =driver.findElement(By.linkText("personal"));
-        link =driver.findElement(By.xpath("//a[@href='https://www.bk.rw/personal']"));
-                link.click();
-              
-                //First click "Current & Saving"
-                link1=driver.findElement(By.xpath("[a[@id=\"nav\"]/div/div/div[2]/u|[1]/li[2]/a"));
-                link1.click();
-                // click on"Personal account link "
-                link2 =driver.findElement(By.xpath("//a[@href='https://www.bk.rw/personal/checking-saving/personal-current']"));
-         link2.click();
+        driver.findElement(By.xpath("//a[@href='https://www.bk.rw/personal']")).click();
+
+ 
+      Float buyingPrice=Float.parseFloat(driver.findElement(By.xpath("/html/body/footer/div[1]/div[4]/div/table/tbody/tr[2]/td[2]")).getText());
+        Float sellingPrice=Float.parseFloat(driver.findElement(By.xpath("/html/body/footer/div[1]/div[4]/div/table/tbody/tr[2]/td[3]")).getText());
+       
+        if((sellingPrice>=800) && (sellingPrice>buyingPrice)){
+            System.out.println("Rates ok");
+            System.out.println(buyingPrice);
+            System.out.println(sellingPrice);
+            
+        }else{
+                        System.out.println("Rates not ok");
+
+        }
+        driver.findElement(By.xpath("//a[@href='https://www.bk.rw/ways-of-banking/online-banking']")).click();
          
-             //Click on online banking link under current  & saving menu
-         link3 =driver.findElement(By.xpath("//a[@href='https://www.bk.rw/ways-of-banking/online-banking']"));
-         link3.click();
-         //get a link for "Apply now button"
-         link4=driver.findElement(By.xpath("//a[@href='https://www.bk.rw/online-services/apply-for-online-banking']"));
-         link4.click();
+        
+      
+        driver.findElement(By.xpath("//a[@href='https://www.bk.rw/personal/checking-saving/personal-current']")).click();
+        
+         
+//Click on online banking link under current & saving menu
+   driver.findElement(By.xpath("//a[@href='https://www.bk.rw/ways-of-banking/online-banking']")).click();
+        
+  
+    Link =driver.findElement(By.xpath("//a[@href='https://www.bk.rw/online-services/apply-for-online-banking']"));
+         link.click();
+      
+        
         try{
             Thread.sleep(5000);
             
         }catch(InterruptedException ex){               
      
  
-            Logger.getLogger(bankOfKigali.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Gloriose.class.getName()).log(Level.SEVERE, null, ex);
         }
         driver.quit();
     }
     
 }
+
+
